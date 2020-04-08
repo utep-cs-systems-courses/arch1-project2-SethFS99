0699b0377a4b73abe1563a5b959892fde7e21118
Seth Flores <sethfloresgamerg33k@gmail.com>
2020-04-08 14:21:36 -0600

    the now final working version of my buzzy toy

--- project/demos/button_demo/stateMachines.s
+++ project/demos/button_demo/stateMachines.s
@@ -1,36 +1,42 @@
 	.arch msp430g2553
+	.p2align 1,0
 	.data
-	S word 0000		;State variable S goes from 0-3
 
-	
-	.text			;my states 0 - 3
-	.word case0:
-	.word case1:
-	.word case2:
-	.word case3:
+State:
+	.word 1		;State variable S goes from 0-3
 
+	.text
+jt:
+	.word default
+	.word option1
+	.word option2
+	.word option3
+	.word option4
 	.global dim_advance
 dim_advance:
-	CMP S,#0
-	JEQ case0
-	CMP S,#1
-	JEQ case1
-	CMP S,#2
-	JEQ case2
-	CMP S,#3
-	JEQ case3
-
-case0:
-	mov #1,S
-	ret 0			;change s and update led
-
-case1:
-	move#2,S
+	CMP #4,&State
+	JHS default
+	mov &State,r12
+	add r12,r12
+	mov jt(r12),r0
+option1:
+	mov #2,&State
 	ret 0
-
-case2:
-	mov #3,S
+	jmp out
+option2:
+	mov #3,&State
 	ret 0
-case3:
-	mov #0,S
+	jmp out
+option3:
+	mov #4,&State
+	ret 0
+	jmp out
+option4:
+	mov #1,&State
 	ret BIT0
+	jmp out
+default:
+	mov #1,&State
+	ret 0
+out:
+	ret 0
